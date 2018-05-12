c934b4fd07fec6d212cc5a606bec1e95f58ec3b8
Taylor Williams <taylor@thewilliams.net>
2018-05-12 18:38:03 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
 # Write your code here.
+def take_a_number(katz_deli, name)
+  katz_deli.push(name)
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+end
+
+def line(katz_deli)
+  
+end
+
+def now_serving(katz_deli)
+  
+end
