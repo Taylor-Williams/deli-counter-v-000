ed219eb7d415cf2f803c99e80eb79471cddc4e6d
Taylor Williams <taylor@thewilliams.net>
2018-05-12 18:48:57 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,9 +8,9 @@ def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
   else
-    queue = "The line is currently: "
+    queue = "The line is currently:"
     katz_deli.each_with_index do |name, index|
-      queue << "#{index + 1}. #{name} "
+      queue << " #{index + 1}. #{name}"
     end
     puts queue
   end
