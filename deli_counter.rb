6f3b8cff899b532166d3f9f18c835475b0f8f80b
Taylor Williams <taylor@thewilliams.net>
2018-05-12 18:43:16 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,14 @@ def take_a_number(katz_deli, name)
 end
 
 def line(katz_deli)
-
+  if katz_deli.length == 0
+    puts "The line is currently empty."
+  else
+    print "The line is currently: "
+    katz_deli.each_with_index do |name, index|
+      print "#{index}. #{name} "
+    end
+    puts ""
 end
 
 def now_serving(katz_deli)
